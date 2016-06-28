f3b4d70446e48f2f0fe3df0f40b71696014e7a6c
Sanket <sanket.parmar11@gmail.com>
2016-06-28 23:49:15 +0530

    Started plugin execution

--- bp-activity-share.php
+++ bp-activity-share.php
@@ -28,3 +28,27 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-bp-activity-share.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_bp_activity_share() {
+
+	$bp_activity_share = new BP_Activity_Share();
+	$bp_activity_share->run();
+
+}
+
+run_bp_activity_share();
